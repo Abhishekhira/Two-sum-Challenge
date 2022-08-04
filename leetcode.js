5a78013e02bdc774801af98fb066e4bdb164d13d
Abhishek hira <92227765+Abhishekhira@users.noreply.github.com>
2022-08-04 14:49:18 +0530

    Add files via upload

---
+++ leetcode.js
@@ -0,0 +1,38 @@
+// Example 1:
+
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+// Example 2:
+
+// Input: nums = [3,2,4], target = 6
+// Output: [1,2]
+// Example 3:
+
+// Input: nums = [3,3], target = 6
+// Output: [0,1]
+
+
+function returnOut(nums) {
+    let a=[]
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = 1; j < nums.length; j++) {
+            if (nums[i]+nums[j]===9 ) {
+               a.push(i)
+            a.push(j)
+           
+        }  else if(nums[i]+nums[j]===6){
+            a.push(i)
+            
+        }
+        
+    }
+    
+}
+console.log(a)
+
+}
+returnOut([2,7,11,15])
+returnOut([3,2,4])
+returnOut([3,3])
+
